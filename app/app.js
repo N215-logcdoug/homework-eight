81fa84debc0efde0f0b546aa87c38cfbb608bc53
Aero48 <56200404+Aero48@users.noreply.github.com>
2022-04-04 23:33:10 -0400

    init commit

---
+++ app/app.js
@@ -0,0 +1,159 @@
+var homeContent = `<div class="hero" style="background-image: url(images/liminal01.png);">
+<p class="hero-title">Liminal World</p>
+</div>
+<div class="home-body">
+<div class="home-body-text">
+    <h1>What are liminal spaces?</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta consequatur culpa necessitatibus fuga cumque reiciendis laborum officiis possimus! Ratione suscipit similique laborum minima quo. Et quibusdam minima eos accusamus nihil.</p>
+</div>
+<div class="home-body-image"></div>
+
+</div>
+<div class="location-list">
+<p class="locations-title">Locations List</p>
+<iframe src="https://docs.google.com/document/d/e/2PACX-1vQ8u0lA0donr5vm_LP1u4wdFQL44D8eR0zwYa2chtz5svdjq5UqztgihGmmyNX9_mNUfcxp2OYuiIPg/pub?embedded=true"></iframe>
+</div>`;
+
+var aboutContent = `<div class="hero" style="background-image: url(images/liminal03.png);">
+<p class="hero-title">Builders</p>
+</div>
+<div class="builders">
+<div class="builder">
+    <div class="builder-image" style="background-image: url(images/HOJL.png)"></div>
+    <div class="builder-text">
+        <h2>Logan</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sequi excepturi. Reiciendis id quam odit rerum nihil sunt fugiat molestias voluptates? Nulla in nobis voluptate. Assumenda iure delectus laudantium quo?</p>
+    </div>
+</div>
+<div class="builder">
+    <div class="builder-image" style="background-image: url(images/predator_logo.png)"></div>
+    <div class="builder-text">
+        <h2>Leon</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sequi excepturi. Reiciendis id quam odit rerum nihil sunt fugiat molestias voluptates? Nulla in nobis voluptate. Assumenda iure delectus laudantium quo?</p>
+    </div>
+</div>
+<div class="builder">
+    <div class="builder-image" style="background-image: url(images/xbox_veer.png)"></div>
+    <div class="builder-text">
+        <h2>Aidan</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sequi excepturi. Reiciendis id quam odit rerum nihil sunt fugiat molestias voluptates? Nulla in nobis voluptate. Assumenda iure delectus laudantium quo?</p>
+    </div>
+</div>
+<div class="builder">
+    <div class="builder-image" style="background-image: url(images/Blarrgensnorf.jpg)"></div>
+    <div class="builder-text">
+        <h2>Elliott</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sequi excepturi. Reiciendis id quam odit rerum nihil sunt fugiat molestias voluptates? Nulla in nobis voluptate. Assumenda iure delectus laudantium quo?</p>
+    </div>
+</div>
+<div class="builder">
+    <div class="builder-image" style="background-image: url(images/Hypervolt.png)"></div>
+    <div class="builder-text">
+        <h2>Hayden</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sequi excepturi. Reiciendis id quam odit rerum nihil sunt fugiat molestias voluptates? Nulla in nobis voluptate. Assumenda iure delectus laudantium quo?</p>
+    </div>
+</div>
+<div class="builder">
+    <div class="builder-image" style="background-image: url(images/monochrome_feudal_japan_3.png)"></div>
+    <div class="builder-text">
+        <h2>Alli</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, sequi excepturi. Reiciendis id quam odit rerum nihil sunt fugiat molestias voluptates? Nulla in nobis voluptate. Assumenda iure delectus laudantium quo?</p>
+    </div>
+</div>
+</div>`;
+
+var modpackContent = `<div class="hero" style="background-image: url(images/liminal04.png); background-color: rgba(0,0,0,0.3);">
+<p class="hero-title">MODPACK</p>
+</div>
+<div class="modpack-download">
+<p>download modpack + world - </p>
+<a href="#"><div class="download-button">
+    <p class="button-text">download  <i class="fa-solid fa-download"></i></p>
+</div></a>
+</div>
+<div class="mod-list">
+<h1>modlist</h1>
+<div class="mod">
+    <div class="mod-icon" style="background-image: url(images/mod-icons/chisel.png)"></div>
+    <p class="mod-name">Chisel</p>
+</div>
+<div class="mod">
+    <div class="mod-icon" style="background-image: url(images/mod-icons/embellish.png)"></div>
+    <p class="mod-name">EmbellishCraft</p>
+</div>
+<div class="mod">
+    <div class="mod-icon" style="background-image: url(images/mod-icons/bop.png)"></div>
+    <p class="mod-name">Biomes O'Plenty</p>
+</div>
+<div class="mod">
+    <div class="mod-icon" style="background-image: url(images/mod-icons/quark.png)"></div>
+    <p class="mod-name">Quark</p>
+</div>
+<div class="mod">
+    <div class="mod-icon" style="background-image: url(images/mod-icons/dustrial.png)"></div>
+    <p class="mod-name">'Dustrial Decor</p>
+</div>
+</div>
+`;
+
+var galleryContent = `<div class="hero" style="background-image: url(images/liminal05.png);">
+<p class="hero-title">gallery</p>
+</div>
+
+<div class="gallery">
+<div class="gallery-row">
+    <div class="gallery-item" style="background-image: url(images/liminal01.png)">
+        <p class="item-text">Luxury Apartments</p>
+    </div>
+    <div class="gallery-item" style="background-image: url(images/liminal02.png)">
+        <p class="item-text">Yellow Halls</p>
+    </div>
+    <div class="gallery-item" style="background-image: url(images/liminal03.png)">
+        <p class="item-text">Waterpark</p>
+    </div>
+</div>
+<div class="gallery-row">
+    <div class="gallery-item" style="background-image: url(images/liminal04.png)">
+        <p class="item-text">Parking Garage</p>
+    </div>
+    <div class="gallery-item" style="background-image: url(images/liminal05.png)">
+        <p class="item-text">Desert Tube</p>
+    </div>
+    <div class="gallery-item" style="background-image: url(images/liminal06.png)">
+        <p class="item-text">Pool Room</p>
+    </div>
+</div>
+<div class="gallery-row">
+    <div class="gallery-item" style="background-image: url(images/liminal07.png)">
+        <p class="item-text">Pizzaria</p>
+    </div>
+    <div class="gallery-item" style="background-image: url(images/liminal08.png)">
+        <p class="item-text">The Mall</p>
+    </div>
+    <div class="gallery-item" style="background-image: url(images/liminal09.png)">
+        <p class="item-text">Watertower</p>
+    </div>
+</div>
+</div>`
+
+function initListeners(){
+    $("nav a").click(function(e){
+        let btnID = e.currentTarget.id;
+        loadPageContent(btnID);
+    });
+}
+
+function loadPageContent(pageName){
+    let contentID = pageName + "Content"
+    $("#app").html(eval(contentID));
+}
+
+function initApp(){
+    initListeners();
+}
+
+
+$(document).ready(function(){
+    loadPageContent("home");
+    initApp();
+})
